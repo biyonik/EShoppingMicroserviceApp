bc65cc1c81d4bdebeebc85cd501528ea1c4335fa
ahmet <ahmet.altun60@gmail.com>
2023-03-15 02:01:59 +0300

    Startup dosyası oluşturuldu

--- Services/Catalog/Catalog.API/Startup.cs
+++ Services/Catalog/Catalog.API/Startup.cs
@@ -2,5 +2,31 @@
 
 public class Startup
 {
-    
+    private readonly IConfiguration _configuration;
+
+    public Startup(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseRouting();
+        app.UseStaticFiles();
+        app.UseAuthorization();
+        app.UseEndpoints((IEndpointRouteBuilder endpoints) =>
+        {
+            endpoints.MapControllers();
+        });
+    }
 }
